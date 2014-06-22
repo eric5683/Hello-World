ff63c5c6f165d2a34bf377f9c90e35322d50dcf1
Eric Sanchez <Eric@Erics-MacBook-Air.local>
2014-06-22 16:36:44 -0400

    Initial Commit

---
+++ Hello World/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by Eric Sanchez on 6/22/14.
+//  Copyright (c) 2014 Eric. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
